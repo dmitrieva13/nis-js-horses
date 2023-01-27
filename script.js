76e949b96d8343760df303e075d9f1ca6e779dfb
Varvara <vadmitrieva@edu.hse.ru>
2023-01-28 02:03:09 +0300

    commit 1

---
+++ script.js
@@ -0,0 +1,442 @@
+const data = {
+    "articles": [
+        {
+            "title": "Правила конкура",
+            "img": "https://sportishka.com/uploads/posts/2021-12/thumbs/1639621480_47-sportishka-com-p-konkur-konnii-sport-sport-krasvivo-foto-50.jpg",
+            "text": `
+            Как и любые другие виды спорта и конкурсы, у такого вида конного 
+            спорта, как конкур, есть ряд правил и требований к проведению. 
+            Путь лошади и ее всадника предполагает дистанцию с барьерами, 
+            которую они оба должны проскакать без остановок, не задевая 
+            конструкции и удерживаясь верхом. Заранее все препятствия 
+            расставляются в четко продуманном порядке, различаясь по ширине и 
+            высоте обустройства. 
+            <br><br>
+            Препятствия по ширине — специальные канавы, которые вырыты в ширину 
+            от 2,5 до 4 м. Иногда вместо канав могут быть специальные шлагбаумы, 
+            который изготавливается из переплетений в виде треугольника.
+            <br>
+            Препятствия по высоте — это могут быть всевозможные барьеры, 
+            например, стенка, крестовина, калитка, их высота должна варьировать 
+            в пределах 0,8-2,2 м. 
+            <br><br>
+            Лошадь должна преодолеть такие «помехи» без прикосновения к ним, 
+            если перед прыжком она сделает дополнительный круг движений, 
+            судья начисляет штрафные 4 очка. Если в течение 3-4 попыток животное
+             не справляется с прыжком и отказывается преодолеть барьер, а также
+              в случае падения всадника, команда выбывает из сражения. К тому 
+              же руководство состязаний дает каждой команде отрезок времени, за
+               который они должны побороть дистанцию. За каждую лишнюю секунду 
+               времени рефери начисляет четверть штрафного балла. Также если
+                всадник и его конь не справятся с отведенным временем, команде 
+                грозит дисквалификация.
+            `,
+            "description": "Как проводятся соревнования по конкуру? Узнайте здесь!",
+            "tags": ["#соревнования"],
+            "id": 1,
+            "read": false,
+            "readLater": false,
+            "difficulty": 1,
+            "sportKind": "Конкур",
+            "date": "2022-12-11T22:11:37.852Z"
+        },
+        {
+            "title": "Уход перед соревнованиями",
+            "img": "https://thypix.com/wp-content/uploads/horse-143.jpg",
+            "text": `Тренировки, безусловно, очень важны. Если нарушать их 
+            последовательность, а также рекомендованные правила проведения, 
+            можно не только не добиться нужного результата, но и получить 
+            довольно неприятные последствия в виде травм у животного и его 
+            плохого состояния. 
+            Однако важнее всего правильно дозировать работу, отдых и уход. 
+            Комфортные условия содержания, а также оптимальные рацион и время 
+            кормления благоприятно сказываются на здоровье лошади и ее внешнем 
+            виде. Следует отметить, что лошадь, которую хорошо содержат и 
+            правильно кормят перед соревнованиями, никогда не худеет, несмотря 
+            ни на какие физические нагрузки и тренировки. А вот скудная 
+            неполноценная пища и несвоевременный отдых вполне могут вывести 
+            лошадь из строя.
+            <br>
+            Если животное довольно уходом, оно выглядит бодрым, имеет блестящую 
+            шерсть, гладкое тело, лошадь набирает вес, у нее укрепляется 
+            организм, особенно легкие и сердце – органы, которые крайне важны 
+            для соревнований.
+            <br>
+            Итак, чтобы лошадь развивалась нормально физиологически и ей 
+            хватало запасов для восстановления энергии после тренировок, важно 
+            не только подобрать определенное количество корма, но и следить за 
+            тем, как он переваривается (усваивается). Поэтому кормить лошадь в 
+            период подготовки к соревнованиям нужно исходя из ряда условий. Для 
+            расчета суточного рациона, как правило, берут во внимание такие 
+            показатели животного, как рост, степень нагрузки, внешний вид, 
+            способность усваивать корм. У лошади должен присутствовать аппетит, 
+            т.е. каждый раз она должна с удовольствием поедать корм, а также 
+            полностью переваривать пищу. Если корм плохо переваривается, это несложно определить.
+            <br>
+            Что же нужно делать для хорошей усвояемости корма? Следовать режиму питания и скармливания. В период тренировок лошади предлагается не только сено, но и зерно. Оно дается три раза в день, естественно, равными порциями. Сено дается небольшими охапками через равные промежутки времени, скажем, 2 часа.
+            <br>
+            Кормить или поить лошадь прямо перед тренировкой или после нее не рекомендуется. Поэтому если работа начнется ранним утром, то и давать корм животному не нужно, а вечернюю тренировку целесообразно проводить спустя час после кормления.
+            <br>
+            Конечно, качество фуража должно быть высоким, чтобы обеспечить животному хорошее восстановление сил и энергии, а также способствовать правильной работе пищеварительной системы.`,
+            "description": "Вы и ваша лошадь готовитесь к соревнованиям по выездке? Узнайте, как сделать это правильно.",
+            "tags": ["#соревнования", "#уход", "#советы"],
+            "id": 2,
+            "read": false,
+            "readLater": false,
+            "difficulty": 3,
+            "sportKind": "Выездка",
+            "date": "2022-12-11T22:12:37.852Z"
+        },
+        {
+            "title": "Одежда для конкура",
+            "img": "https://krasivosti.pro/uploads/posts/2021-04/1618218657_32-p-loshadinie-skachki-loshadi-krasivo-foto-36.jpg",
+            "text": `По правилам с утра перед конкуром всадник и его лошадь 
+            должны быть обмундированы в презентабельные наряды. Для этого члены 
+            клубов и ассоциаций свою команду снаряжают в специальный костюм, 
+            чтобы те легко выделялись во время прохождения дистанции. Если 
+            личного костюма нет в наличии, стандартный набор состоит из черного 
+            либо синего редингтона, а также белого шарфа для женщины-всадницы, 
+            а для мужчины-наездника — алый редингтон и галстук белого цвета. 
+            <br>
+            Если это международные схватки по конкуру, национальную 
+            принадлежность можно узнать по оттенку наряда. Например, ирландский 
+            всадник снаряжается в зеленый редингтон, в обязательном порядке 
+            должны быть кепи синего или черного цвета.
+            <br>
+            Разрешены также хлысты, но не длиннее установленной нормы — 750 мм. 
+            Допустимы также шпоры, которые носит всадник, но острые концы их 
+            должны смотреть либо вниз, либо в наружную сторону. Обязательное 
+            условие ко всем участникам конкура — сапоги специальные для 
+            скачек, бриджи и рубашка. Если будет жаркая погода, организаторы 
+            могут разрешить не надевать редингтон. Под запретом такие вещи, как 
+            рубашка-поло, куртка без рукавов, а также пуловеры. Если 
+            предвидится непогода, можно надеть плащ. Если в конкуре участвуют 
+            военные либо полицейские, они могут обличить себя в личную форму. 
+            Детские конкуры предполагают легкую, практичную и неброскую одежду.`,
+            "description": "Какая существует форма для соревнований?",
+            "tags": ["#снаряжение", "#соревнования"],
+            "id": 3,
+            "read": false,
+            "readLater": false,
+            "difficulty": 2,
+            "sportKind": "Конкур",
+            "date": "2022-12-11T22:13:37.852Z"
+        },
+        {
+            "title": "Cleany - лучшие щетки для ваших любимцев",
+            "img": "https://krasivosti.pro/uploads/posts/2021-04/1618214089_6-p-loshadki-poni-loshadi-krasivo-foto-6.jpg",
+            "text": `Инновационные щетки из серии Cleany PonyUp Brush разработаны специально с учетом физических особенностей малышей-пони. Щетки бережно очищают шерсть пони, не повреждая нежную кожу. Заказывайте сейчас на нашем сайте!`,
+            "description": "Подарите вашим пони самую гладкую шерстку с помощью наших уникальных щеток",
+            "tags": ["#реклама", "#уход", "#снаряжение"],
+            "id": 4,
+            "read": false,
+            "readLater": false,
+            "difficulty": 3,
+            "sportKind": null,
+            "date": "2022-12-11T23:13:37.852Z"
+        }
+    ]
+  }
+
+  const tags = ["#снаряжение", "#уход", "#соревнования", "#новости", "#реклама",
+"#советы"]
+
+
+// загрузка всех постов из массива data
+const loadPosts = (data) => {
+    data.articles = data.articles.sort((a, b) => {
+        return Date.parse(a.date) > Date.parse(b.date) ? -1 : 1
+    })
+
+    let main = document.querySelector(".main")
+
+    document.querySelectorAll(".post").forEach(post => {
+        main.removeChild(post)
+    })
+
+    data.articles.forEach(e => {
+        var div = document.createElement('div');
+        div.className = "post"
+        div.id = e.id
+        let difficulty = "???"
+        switch(e.difficulty){
+            case 0:
+                difficulty = null
+                break
+            case 1:
+                difficulty = "Новичок"
+                break
+            case 2:
+                difficulty = "Любитель"
+                break
+            case 3:
+                difficulty = "Профессионал"
+                break
+        }
+        div.innerHTML = `
+            <div class="postInfo">
+                <div class="difficulty" style="display:${e.difficulty == 0 ? 'none' : 'block'}">
+                    ${difficulty}
+                </div>
+                <div class="sportKind" style="display:${e.sportKind == null ? 'none' : 'block'}">
+                    ${e.sportKind}
+                </div>
+            </div>
+
+            <img src="${e.img}" />
+            <div class="postTitle">${e.title}</div>
+            <div class="postDescription">${e.description}</div>
+            <div class="tagsPanel">${e.tags.join(' ')}</div>
+        `
+        main.appendChild(div)
+    });
+
+    addPostsListeners()
+};
+
+// поиск поста по id
+const findPost = id => {
+    return data.articles.filter(post => post.id == id)[0]
+}
+
+// добавление статьи в список "прочитать позже"
+const readLaterAdd = id => {
+    let oldVal = localStorage.getItem('readLater')
+    let newVal = oldVal + '{' + id + '}'
+    localStorage.readLater = newVal
+}
+
+// удаление статьи из списка "прочитать позже"
+const readLaterDelete = id => {
+    let oldVal = localStorage.getItem('readLater')
+    let del = '{' + id + '}'
+    newVal = oldVal.replace(del, '')
+    localStorage.readLater = newVal
+}
+
+// проверка наличия статьи в списке "прочитать позже"
+const readLaterContains = id => {
+    let idStr = '{' + id + '}'
+    return localStorage.getItem('readLater').includes(idStr)
+}
+
+// добавление статьи в список прочитанных
+const isReadAdd = id => {
+    let oldVal = localStorage.getItem('isRead')
+    let newVal = oldVal + '{' + id + '}'
+    localStorage.isRead = newVal
+}
+
+// удаление статьи из списка прочитанных
+const isReadDelete = id => {
+    let oldVal = localStorage.getItem('isRead')
+    let del = '{' + id + '}'
+    newVal = oldVal.replace(del, '')
+    localStorage.isRead = newVal
+}
+
+// проверка наличия статьи в списке прочитанных
+const isReadContains = id => {
+    let idStr = '{' + id + '}'
+    return localStorage.getItem('isRead').includes(idStr)
+}
+
+// получение массива с id всех статей в списке "прочитать позже"
+const getReadLater = () => {
+    let arr = []
+    data.articles.forEach(a => {
+        if (readLaterContains(a.id)) {
+            arr.push(a.id)
+        }
+    })
+    return arr
+}
+
+// получение массива с id всех статей в списке прочитанных
+const getRead = () => {
+    let arr = []
+    data.articles.forEach(a => {
+        if (isReadContains(a.id)) {
+            arr.push(a.id)
+        }
+    })
+    return arr
+}
+
+// конвертация времени из формата ISO-строки в строку с датой и временем
+function timeConverter(iso_time){
+    let converted = new Date(iso_time)
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = converted.getFullYear()
+    let month = months[converted.getMonth()]
+    let date = converted.getDate()
+    let hour = converted.getHours()
+    let min = converted.getMinutes()
+    let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min
+    return time
+}
+
+// функция при нажатии на кнопку "добавить в прочитанное" (или удалить)
+function toggleRead(id){
+    if (isReadContains(id)) {
+        isReadDelete(id)
+    } else {
+        isReadAdd(id)
+        filterApply()
+    }
+    document.querySelector(".readButton").innerHTML = 
+        `
+            ${isReadContains(id) ? "Удалить из прочитанных" : "Пометить как прочитанное"}
+        `
+}
+
+// функция при нажатии на кнопку "добавить в прочитать позже" (или удалить)
+function toggleReadLater(id){
+    if (readLaterContains(id)) {
+        readLaterDelete(id)
+        filterApply()
+    } else {
+        readLaterAdd(id)
+    }
+    document.querySelector(".readLaterButton").innerHTML = 
+        `
+        ${readLaterContains(id) ? "Удалить из списка для прочтения" : "Добавить в прочесть позже"}
+        `
+}
+
+// функция, меняющая надпись выбранного уровня при изменении значения слайдера
+function slider(){
+    let value = document.querySelector("#levelSlider").value
+    let levelText = document.querySelector("#levelText")
+    levelText.innerHTML = value
+    switch(value){
+        case '0':
+            levelText.innerHTML = "Все статьи"
+            break
+        case '1':
+            levelText.innerHTML = "Новичок"
+            break
+        case '2':
+            levelText.innerHTML = "Любитель"
+            break
+        case '3':
+            levelText.innerHTML = "Профессионал"
+            break
+    }
+}
+
+// добавление  event listener ко всем постам, чтобы при нажатии на пост открывалась статья
+function addPostsListeners(){
+    document.querySelectorAll(".post").forEach(post => {
+        post.addEventListener("click", e => {
+            scrollTo = window.scrollY
+            floatingPanel.style.opacity = "1"
+            floatingPanel.style.pointerEvents = "all"
+
+            document.querySelector(".main").style.maxHeight = "100vh"
+            document.querySelector(".main").style.overflow = "hidden"
+
+            let data = findPost(post.id)
+
+            document.querySelector(".article").innerHTML = 
+            `
+                <img class="articleImg" src="${data.img}" />
+                <div class="articleTitle">${data.title}</div>
+                <div className = "articleText">
+                    ${data.text}
+                </div>
+                <div class="articleDate">${timeConverter(data.date)}</div>
+                <button class="readButton" onclick="toggleRead(${data.id})">
+                    ${isReadContains(data.id) ? "Удалить из прочитанных" : "Пометить как прочитанное"}
+                </button>
+                <button class="readLaterButton" onclick="toggleReadLater(${data.id})">
+                    ${readLaterContains(data.id) ? "Удалить из списка для прочтения" : "Добавить в прочесть позже"}
+                </button>
+            `
+        })
+    })
+}
+
+// применение фильтров к статьям
+function filterApply(){
+    let dataToShow = {
+        articles: []
+    }
+
+    // выбираем виды спорта
+    let active = document.querySelectorAll(".sportActive")
+    active.forEach(sport => {
+        data.articles.forEach(article => {
+            if(article.sportKind == sport.innerHTML){
+                dataToShow.articles.push(article)
+            }
+        })
+    })
+    if(active.length == 0){
+        dataToShow.articles = data.articles
+    }
+
+    // выбираем статьи с отмеченными тегами
+    let tagsActive = document.querySelectorAll(".tagActive")
+    tagsActive = Array.from(tagsActive).map(t => t.innerHTML)
+    console.log(tagsActive)
+    
+    if (tagsActive.length != 0) {
+        dataToShow.articles = dataToShow.articles.filter(a => {
+            let contains = true
+            tagsActive.forEach(t => {
+                if (!a.tags.includes(t)) {
+                    contains = false
+                }
+            })
+            return contains
+        })
+    }
+
+    // выбираем уровень сложности
+    let levelValue = document.getElementById("levelSlider").value
+    if(levelValue != 0){
+        dataToShow.articles = dataToShow.articles.filter(a => {
+            return a.difficulty == levelValue
+        })
+    }
+
+    // скрываем прочитанные статьи, если это отмечено
+    if (document.querySelector('#hideRead:checked')) {
+        let read = getRead()
+        dataToShow.articles = dataToShow.articles.filter(a => {
+            return !read.includes(a.id)
+        })
+    }
+
+    // показываем только статьи из "прочитать позже", если это отмечено
+    if (document.querySelector('#showReadLater:checked')) {
+        let readLater = getReadLater()
+        dataToShow.articles = dataToShow.articles.filter(a => {
+            return readLater.includes(a.id)
+        })
+    }
+
+    loadPosts(dataToShow)
+}
+
+// сброс всех фильтров
+function filterReset(){
+    let active = document.querySelectorAll(".sportActive")
+    active.forEach(sport => {
+        sport.classList.remove("sportActive")
+    })
+
+    active = document.querySelectorAll(".tagActive")
+    active.forEach(tag => {
+        tag.classList.remove("tagActive")
+    })
+
+    document.getElementById("levelSlider").value = 0
+    document.querySelector("#levelText"). innerHTML = "Все статьи"
+
+    document.getElementById("hideRead").checked = false
+    document.getElementById("showReadLater").checked = false
+
+    loadPosts(data)
+}
